012e069681400a34c90e0e82964e50650d903da9
TiBiscuit95 <98490387+TiBiscuit95@users.noreply.github.com>
2023-10-18 16:19:07 +0200

    presque fini

--- Application.py
+++ Application.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from PageAccueil import PageAccueil
-from tkinter import ttk
+
 
 class Application(tk.Tk):
    def __init__(self, title, width, height):
@@ -13,5 +13,4 @@ class Application(tk.Tk):
         self.minsize(width, height)
         self.config(bg = "#87CEEB")
         self.page_accueil = PageAccueil(self)
-        self.page_accueil.pack(fill=tk.BOTH, expand=True)
-
+        self.page_accueil.pack()
