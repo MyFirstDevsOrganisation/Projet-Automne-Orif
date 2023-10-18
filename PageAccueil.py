e55f4ec03a57f4fc702d7610c1a4895db3679fae
TiBiscuit95 <98490387+TiBiscuit95@users.noreply.github.com>
2023-10-18 13:56:21 +0200

    Implémentaton boutton quitter

--- PageAccueil.py
+++ PageAccueil.py
@@ -1,9 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
-from tkinter import Frame
-import PageJeu
-
+from PageJeu import PageJeu
 
 
 class PageAccueil(tk.Frame):
@@ -11,19 +9,23 @@ class PageAccueil(tk.Frame):
         super().__init__(parent)
 
         self.createWidgets()
+        
 
     def createWidgets(self):
-        explicationLabel = tk.Label(self, text="Choisissez le type de verbe que vous voulez reviser", font=("Helvetica", 15))
-        demarrerBoutton = tk.Button(self, text = "Start", height=5, width=10, command=self.afficherPageJeu)
-
-
+        explicationLabel = tk.Label(self, text="Choisissez le type de verbe que vous voulez reviser", font=("Helvetica", 15), bg='#87CEEB')
         explicationLabel.grid(row=5, column=1, columnspan=2, sticky=tk.NSEW, pady=30)
+
+        demarrerBoutton = tk.Button(self, text = "Start", height=5, width=10, command=self.afficherPageJeu)
         demarrerBoutton.grid(row=7, column=0, columnspan=2)
 
+        quitterBoutton = tk.Button(self, text= "Quitter", height=5, width=10, command=self.confirmationQuitter)
+        quitterBoutton.grid(row=9, column=2, columnspan=2)
 
     def afficherPageJeu(self):
         self.pack_forget()
-        self.page_jeu = PageJeu()
+        self.page_jeu = PageJeu(self.master)
 
-
-
+    def confirmationQuitter(self):
+        reponse = messagebox.askokcancel('Quitter', 'Voulez-vous vraiment quitter ?')
+        if reponse:
+            self.destroy()
