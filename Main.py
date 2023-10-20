422575438763d11d4c230d8934b16fcc8352eeac
TiBiscuit95 <98490387+TiBiscuit95@users.noreply.github.com>
2023-10-20 12:59:16 +0200

    Changement taille fenêtre et icone

--- Main.py
+++ Main.py
@@ -4,5 +4,6 @@ from Application import Application
 
 if __name__ == "__main__":
    
-   app = Application("Revision des verbes en Anglais", 600, 600)
+   app = Application("Revision des verbes en Anglais", 800, 600)
+   app.iconbitmap('c:/Users/romai/Downloads/EnglishLanguage_Flag1_26107.ico')
    app.mainloop()
