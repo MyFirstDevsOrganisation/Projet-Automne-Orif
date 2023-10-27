2e31a6ac69ef350571836754fddd672bbdd70421
Greg_VonFisch <132044931+VonFisch@users.noreply.github.com>
2023-10-27 13:21:17 +0200

    Update Main.py
    Ajout de commentaires

--- Main.py
+++ Main.py
@@ -1,3 +1,11 @@
+# Orif section informatique
+# Exercice de groupe
+# Projet Automne 
+# Le programme est conçu pour réviser les verbes irréguliers en anglais sous forme de formulaire
+#
+# Auteurs : Dylan Diaz, Romain Howald, Loris Bayard, Grégoire Fischer, Nedeljko Mathieu (consultant/Observateur), Fabien Ducris (Contrôleur)
+# Date   : 27.10.23
+
 # import des librairies
 import tkinter as tk
 from tkinter import *
